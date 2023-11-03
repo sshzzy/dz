8aaa1ef653dc6501a00d17775d28c9f6e553f625
sshzzy <149818309+sshzzy@users.noreply.github.com>
2023-11-03 20:06:25 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+   <HeaderComponent></HeaderComponent>
+   <MainComponent></MainComponent>
+
+</template>
+
+
+<script>
+import MainComponent from "./grandParentsComponents/MainView.vue";
+import HeaderComponent from './grandParentsComponents/HeaderView.vue'
+
+export default {
+  name: 'App',
+  components:{
+    MainComponent,
+    HeaderComponent 
+
+
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 1400px;
+  display: flex;
+  justify-content: center;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from "./grandParentsComponents/router";
 
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
