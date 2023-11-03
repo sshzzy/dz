798f0a920ff14c729143a3df610059580338c8c2
sshzzy <149818309+sshzzy@users.noreply.github.com>
2023-11-03 20:26:35 +0300

    Add files via upload

---
+++ src/grandParentsComponents/HeaderView.vue
@@ -0,0 +1,44 @@
+<template >
+    <main>       
+        <div class="Links">
+            <section>        
+                <router-link :to="({name: 'home'})" class="isActive">Твоя домашняя страница</router-link>
+            </section>
+            <section>
+                <router-link :to="({name: 'chat'})" class="isActive">Чаты</router-link>
+            </section>
+            <section>
+                <router-link :to="({name: 'maps'})" class="isActive">Карта</router-link>
+            </section>
+        
+        </div>
+        <section>
+            <RouterView/>
+        </section>
+    </main>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .Links{
+        width:800px;
+        display:flex;
+        justify-content: space-between;
+    }
+    .isActive{
+        text-decoration: none;
+        color: azure;
+        width: 200px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: rgba(31, 31, 31, 0.5);
+        border-radius: 20px;
+        margin: center;
+    }
+</style>
---
+++ src/grandParentsComponents/MainView.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+
+    </main>
+    </template>
+    
+    <script>
+    
+    export default {
+        name: "MainComponent"
+    
+    }
+    </script>
+    
+    <style scoped>
+    main{
+        display: flex;
+        justify-content: center;
+    }
+    </style>
